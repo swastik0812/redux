acc72b0ea5869bea5700d804e4c0f4684fe60ee0
Abhishek Mishra <abhish1.mishra@gmail.com>
2020-07-01 14:28:54 +0530

    check

---
+++ redux-basics.js
@@ -0,0 +1,40 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0,
+};
+//reducer
+const rootReducer = (state = initialState, action) => {
+  if (action.type === "INK_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
+  return state;
+};
+
+//store
+
+const store = createStore(rootReducer);
+
+console.log(store.getState());
+
+store.subscribe(() => {
+  console.log("[subscription]", store.getState());
+});
+
+//Dispatching action
+store.dispatch({ type: "INK_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
+
+//subscription
